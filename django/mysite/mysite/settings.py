26252e1a3dd5e83bbcfede0d38f732fe12686752
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-02-13 14:40:48 +0300

    config commit

--- django/mysite/mysite/settings.py
+++ django/mysite/mysite/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import configparser
+config = configparser.ConfigParser()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+config.read(f'{BASE_DIR}/config.ini')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!fbav($7xgyihgt-tjh34pa6!qtsvchc79)2!=7bz@vk5b5&72'
+SECRET_KEY = config['TOKENS']['TOKEN']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
