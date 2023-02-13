b80658e91c93aaaba4f1085efcf9d20b1fb31041
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-02-13 16:12:12 +0300

    myapplication initial commit

--- django/mysite/mysite/settings.py
+++ django/mysite/mysite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config['TOKENS']['TOKEN']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
