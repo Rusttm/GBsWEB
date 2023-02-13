bb10f01c52d23cd32974ccf5eab02d97f3b35b52
Rustam <52700794+Rusttm@users.noreply.github.com>
2023-02-13 16:42:09 +0300

    try upload file commit

--- django/mysite/webapp/views.py
+++ django/mysite/webapp/views.py
@@ -4,4 +4,21 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
 # Create your views here.
+from django.http import HttpResponseRedirect
+from .forms import UploadFileForm
+
+# Imaginary function to handle an uploaded file.
+from myuploader import handle_uploaded_file
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
